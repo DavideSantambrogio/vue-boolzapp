a9a89d952f24bd7f7ec215380c26bfae46e01f2a
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-16 12:55:34 +0100

    add fixed files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data () {
+        // dati
+        return{
+
+        }
+    },    
+
+    created(){
+        // all'avvio della pagina
+
+    },
+
+    methods: {
+        // funzioni
+          
+    },
+}).mount("#app");
